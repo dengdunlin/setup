1e8e87901716504c6d600f6edd40af79d46ae7f8
Dunlin Deng <dunlin.deng@servicenow.com>
2020-04-29 08:52:37 -0700

    add brew scripts

---
+++ brew.sh
@@ -0,0 +1,18 @@
+#command line tools
+xcode-select install
+
+#brew install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+#brew install
+brew install ack
+brew install wget
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install hyperdock
+brew cask install sourcetree
+brew cask install appcleaner
+brew cask install visual-studio-code 
+brew cask install bitwarden
